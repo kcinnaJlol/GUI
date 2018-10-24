1812cf7cd179d2e20ad9fdb6805d75ab851103bc
kcinnaJlol <39594309+kcinnaJlol@users.noreply.github.com>
2018-10-24 15:01:24 +0200

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -445,8 +445,8 @@ local function containerStartEventHandling(container, eventPullTimeout)
 	container.eventPullTimeout = eventPullTimeout
 
 	repeat
-		e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28, e29, e30, e31, e32 = event.pull(container.animations and 0 or container.eventPullTimeout)
-		
+		e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28, e29, e30, e31, e32 = event.pull(container.animations, 0 or container.eventPullTimeout)
+		if not e1 then return end
 		handle(
 			e1 == "touch" or
 			e1 == "drag" or
@@ -495,7 +495,7 @@ local function containerStartEventHandling(container, eventPullTimeout)
 
 			-- По завершению продрочки отрисовываем изменения на экране
 			container:drawOnScreen()
-
+			if eventPullTimeout then return end
 			-- Вызываем поочередно все методы .onFinish
 			for i = 1, #animationOnFinishMethods do
 				animationOnFinishMethods[i].onFinish(container, animationOnFinishMethods[i])
